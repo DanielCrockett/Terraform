d755df544a6b52a0b8393a4f0d692b5bf449b4e9
VibeSlowly <danielcrockett@outlook.com>
2023-05-31 12:01:48 -0400

    new money

--- main.tf
+++ main.tf
@@ -44,12 +44,17 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route" "public" {
-  route_table_id = aws_route_table.public.id
+  route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.main.id
+  gateway_id             = aws_internet_gateway.main.id
 }
 
-resource "aws_route_table_association" "publicsubs" {
+resource "aws_route_table_association" "public1" {
   route_table_id = aws_route_table.public.id
-  subnet_id = [aws_subnet.publicsub1.id, aws_subnet.publicsub2.id]
+  subnet_id      = aws_subnet.publicsub1.id
+}
+
+resource "aws_route_table_association" "public2" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.publicsub2.id
 }
