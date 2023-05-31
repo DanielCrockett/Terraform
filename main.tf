ac0f4e3eb7974de65baa1e5b793037ec5a526670
VibeSlowly <danielcrockett@outlook.com>
2023-05-31 11:52:44 -0400

    new test

--- main.tf
+++ main.tf
@@ -34,3 +34,22 @@ resource "aws_subnet" "publicsub2" {
     Name = "Akatsuki-pulicsubnet"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+    tags = {
+    Name = "Akatsuki-publicroutetable"
+    env = "Dev"
+    }
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "publicsubs" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = [aws_subnet.public1.id, aws_subnet.public2.id]
+}
