55123699edc01b8b66473d56b45bc78172066249
VibeSlowly <danielcrockett@outlook.com>
2023-05-31 12:06:31 -0400

    change

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "publicsub1" {
 
 resource "aws_subnet" "publicsub2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
